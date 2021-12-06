ad384439bd4c97a253dca3e95e070190871068e8
Israel Gadelha <gdelha.israel@gmail.com>
2021-12-06 17:04:02 -0300

    Feat: Implementações feitas, testes passando

--- src/main/java/com/devsuperior/bds03/services/UserService.java
+++ src/main/java/com/devsuperior/bds03/services/UserService.java
@@ -6,18 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.bds03.entities.User;
 import com.devsuperior.bds03.repositories.UserRepository;
 
-
+@Service
 public class UserService implements UserDetailsService{
 	
 private static Logger logger = LoggerFactory.getLogger(UserService.class);
 		
 	@Autowired
 	private UserRepository repository;
-	
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
